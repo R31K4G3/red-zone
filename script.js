392ffc2bdb94ee557d766289a9d91add49c6e528
R31K4G3 <125360100+R31K4G3@users.noreply.github.com>
2026-01-11 03:04:09 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,126 @@
+const audioElement = document.createElement("audio");
+audioElement.src = "./audio.mp3";
+audioElement.loop = true;
+audioElement.preload = "auto";
+audioElement.style.pointerEvents = "none";
+audioElement.load();
+audioElement.addEventListener("loadedmetadata", () => {
+    updateCurrentTimeDisplay();
+});
+document.body.appendChild(audioElement);
+
+const audioContext = new AudioContext();
+const source = audioContext.createMediaElementSource(audioElement);
+const gainNode = audioContext.createGain();
+gainNode.gain.value = 1;
+source.connect(gainNode);
+gainNode.connect(audioContext.destination);
+
+const controlsSpan = document.createElement("span");
+controlsSpan.style.userSelect = "none";
+controlsSpan.style.background = "#222";
+controlsSpan.style.display = "inline-block";
+controlsSpan.style.padding = "7px";
+
+const playingControl = document.createElement("input");
+playingControl.type = "checkbox";
+playingControl.id = "playing-control";
+playingControl.style.width = "0px";
+playingControl.style.height = "0px";
+controlsSpan.appendChild(playingControl);
+
+const playingDisplaylabel = document.createElement("label");
+playingDisplaylabel.htmlFor = "playing-control";
+const playingDisplay = document.createElement("img");
+playingDisplay.src = "./play.svg";
+playingDisplaylabel.appendChild(playingDisplay);
+controlsSpan.appendChild(playingDisplaylabel);
+
+const currentTimeControl = document.createElement("input");
+currentTimeControl.type = "range";
+currentTimeControl.min = "0";
+currentTimeControl.max = "1024";
+controlsSpan.appendChild(currentTimeControl);
+
+const currentTimeDisplay = document.createElement("span");
+currentTimeDisplay.textContent = "00:00 / 00:00";
+currentTimeDisplay.style.fontSize = "smaller";
+const updateCurrentTimeDisplay = () => {
+    let t = Math.round(audioElement.currentTime);
+    let currentTime = `${t % 60}`.padStart(2, "0");
+    t = Math.floor(t / 60);
+    currentTime = `${t % 60}`.padStart(2, "0") + ":" + currentTime;
+    t = Math.floor(t / 60);
+    if (t) currentTime = `${t}:${currentTime}`;
+
+    t = Math.round(audioElement.duration);
+    let duration = `${t % 60}`.padStart(2, "0");
+    t = Math.floor(t / 60);
+    duration = `${t % 60}`.padStart(2, "0") + ":" + duration;
+    t = Math.floor(t / 60);
+    if (t) duration = `${t}:${duration}`;
+
+    currentTimeDisplay.textContent = `${currentTime} / ${duration}`;
+};
+controlsSpan.appendChild(currentTimeDisplay);
+
+const muteControl = document.createElement("input");
+muteControl.type = "checkbox";
+muteControl.id = "mute-control";
+muteControl.style.width = "0px";
+muteControl.style.height = "0px";
+controlsSpan.appendChild(muteControl);
+
+const muteDisplaylabel = document.createElement("label");
+muteDisplaylabel.htmlFor = "mute-control";
+const muteDisplay = document.createElement("img");
+muteDisplay.src = "./unmuted.svg";
+muteDisplaylabel.appendChild(muteDisplay);
+controlsSpan.appendChild(muteDisplaylabel);
+
+const volumeControl = document.createElement("input");
+volumeControl.type = "range";
+volumeControl.min = "0";
+volumeControl.style.width = "60px";
+volumeControl.max = "1024";
+controlsSpan.appendChild(volumeControl);
+
+requestAnimationFrame(() => {
+    currentTimeControl.value = "0";
+    volumeControl.value = "1024";
+});
+document.body.appendChild(controlsSpan);
+
+const intervalIds = [];
+audioElement.addEventListener("play", () => {
+    intervalIds.push(setInterval(() => {
+        currentTimeControl.value = Math.round(currentTimeControl.max * (audioElement.currentTime / audioElement.duration));
+        updateCurrentTimeDisplay();
+    }, 60));
+});
+audioElement.addEventListener("pause", () => {
+    for (const id of intervalIds.splice(0, intervalIds.length)) {
+        clearTimeout(id);
+    }
+});
+playingControl.addEventListener("input", () => {
+    playingDisplay.src = playingControl.checked ? "./pause.svg" : "./play.svg";
+    audioElement[playingControl.checked ? "play" : "pause"]();
+    audioContext.resume();
+});
+currentTimeControl.addEventListener("input", () => {
+    audioElement.currentTime = audioElement.duration * (currentTimeControl.value / currentTimeControl.max);
+    updateCurrentTimeDisplay();
+});
+muteControl.addEventListener("input", () => {
+    audioElement.muted = muteControl.checked;
+    muteDisplay.src = muteControl.checked ? "./muted.svg" : "./unmuted.svg";
+    if (audioElement.muted) {
+        volumeControl.value = "0";
+    } else {
+        volumeControl.value = Math.round(gainNode.gain.value * volumeControl.max);
+    }
+});
+volumeControl.addEventListener("input", () => {
+    gainNode.gain.value = volumeControl.value / volumeControl.max;
+});
